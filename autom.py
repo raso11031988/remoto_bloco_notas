6ccc4bc36dd36dbdbbc95c7d61d29c187fa2c0f6
raso11031988 <raso11031988@gmail.com>
2026-04-25 02:31:43 -0300

    Comando Blocos de Notas

---
+++ autom.py
@@ -0,0 +1,12 @@
+import pyautogui
+import time
+
+time.sleep(5)
+pyautogui.press("win")
+time.sleep(2)
+pyautogui.write("bloco de notas")
+time.sleep(8)
+pyautogui.press("enter")
+time.sleep(10)
+pyautogui.write("Projetinho")
+
