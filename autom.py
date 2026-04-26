fb3b4da0e8ee910eaa1f0c5999ef3bf23a6ab658
raso11031988 <raso11031988@gmail.com>
2026-04-25 21:37:24 -0300

    Off automatico

--- autom.py
+++ autom.py
@@ -1,12 +1,25 @@
 import pyautogui
 import time
+import subprocess
 
 time.sleep(5)
 pyautogui.press("win")
 time.sleep(2)
 pyautogui.write("bloco de notas")
 time.sleep(8)
-pyautogui.press("enter")
+pyautogui.click(x=402,y=239)
 time.sleep(10)
 pyautogui.write("Projetinho")
+
+time.sleep(10)
+pyautogui.press("win")
+time.sleep(10)
+pyautogui.click(x=826, y=680)
+time.sleep(10)
+pyautogui.moveTo(x=831, y=599)
+time.sleep(6)
+pyautogui.click(x=831, y=599)
+pyautogui.click(x=831, y=599)
+pyautogui.click(x=831, y=599)
+
 
